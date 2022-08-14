2d2891cf29ee47596daec57e1ef2d0ee992b3777
Jacob Adelman <jacobzadelman@gmail.com>
2022-08-14 16:13:12 -0400

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-console.log("hello");
-
+//fetch newscatcher api data
 var url = "https://api.newscatcherapi.com/v2/search?q=bitcoin";
 fetch(url, {
   headers: { "x-api-key": "C1WcsheJv-MBZTYHt7uzmC6lPuJIF_JqpSYI0oTbylw" },
@@ -11,32 +10,46 @@ fetch(url, {
   .then((data) => {
     console.log(data);
     for (let i = 0; i < 10; i++) {
-      var name = ".accordion__item__" + i.toString();
-      var accordionItemEl = $(name);
-      var accordionTitleEl = accordionItemEl.children(".accordion__title");
-      var accordionContentEl = accordionItemEl.children(".accordion__content");
-      // console.log(accordionTitleEl);
-      accordionTitleEl.html(
-        data.articles[i].title +
-          '<div class="bmenu x7"><span class="btop"></span><span class="bmid"></span><span class="bbot"></span></div>'
-      );
+      // get title and content elements
+      var accordionItemElContainer = $(".article_" + i.toString());
+      var findEl = accordionItemElContainer.find(".article-wrap");
+      var accordionTitleEl = findEl.children(".article-title");
+      var accordionContentEl = findEl.children(".section-content");
+
+      //append text to title and content elements
+      accordionTitleEl.html(data.articles[i].title);
       accordionContentEl.text(data.articles[i].summary);
+
+      //fetch translations goes here (might have to use settimeout to delay each fetch)
     }
+
+    // BOILERPLATE TRANSLATION CODE (needs to be added to for loop above)
+    // var newurl = "https://translation.googleapis.com/language/translate/v2?";
+    // fetch(
+    //   newurl +
+    //     new URLSearchParams({
+    //       q: "hello",
+    //       target: "Fr",
+    //       source: "En",
+    //       key: "AIzaSyC-uuEmmJHjs_ghh1d1DdTH3Ks7BaoGCsc",
+    //     })
+    // )
+    //   .then((res) => {
+    //     console.log(res);
+    //     return res.json();
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //   });
 
     return data;
   });
 
-$(function () {
-  $(".accordion")
-    .find(".accordion__title")
-    .click(function () {
-      // Adds active class
-      $(this).toggleClass("active");
-      // Expand or collapse this panel
-      $(this).next().slideToggle("fast");
-      // Hide the other panels
-      $(".accordion__content").not($(this).next()).slideUp("fast");
-      // Removes active class from other titles
-      $(".accordion__title").not($(this)).removeClass("active");
-    });
-});
+var section = $("li");
+
+function toggleAccordion() {
+  section.removeClass("active");
+  $(this).addClass("active");
+}
+
+section.on("click", toggleAccordion);
